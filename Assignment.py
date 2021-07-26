f7b0886e6e765780bdd4a00462cc7dce1fb83837
Leejames2305 <leejames2305@gmail.com>
2021-07-26 13:13:33 +0800

    Update

--- Assignment.py
+++ Assignment.py
@@ -6,3 +6,7 @@ ID = int(input("Please key in your Student ID : "))
 while len(str(ID)) != 7:
     print("Invalid Student ID (Exp of correct Student ID > 2100111)")
     ID = int(input("Please key in your Student ID : "))
+
+print("Student's ID = ", ID)
+print("Name = ")
+print("Stream")
