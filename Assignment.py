0a2049e33c0c74ca1f2903dd084aa9fb42cf9a1c
Leejames2305 <leejames2305@gmail.com>
2021-07-26 13:02:59 +0800

    Test

---
+++ Assignment.py
@@ -0,0 +1,8 @@
+print("----------------------------------------------------------")
+print("              WELCOME TO THE CGPA CALCULATOR              ")
+print("----------------------------------------------------------")
+
+ID = int(input("Please key in your Student ID : "))
+while len(str(ID)) != 7:
+    print("Invalid Student ID (Exp of correct Student ID > 2100111)")
+    ID = int(input("Please key in your Student ID : "))
